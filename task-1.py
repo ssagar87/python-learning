a83d5ef91008fa909a150c2b2ef8a96cc25af774
ssagar87 <103563018+ssagar87@users.noreply.github.com>
2025-04-21 22:25:30 +0530

    Completed 1st assignment

---
+++ task-1.py
@@ -0,0 +1,14 @@
+a = int(input('Please enter a value'))
+b = int(input('Please enter b value'))
+
+addResult = a + b
+print('Addition: ', addResult)
+
+subtractionResult = a - b
+print('Subtraction: ', subtractionResult)
+
+multiplicationResult = a * b
+print('Multiplication: ', multiplicationResult)
+
+divisionResult = a * b
+print('Division: ', divisionResult)
---
+++ task-2.py
@@ -0,0 +1,4 @@
+firstName = input('Please enter first name: ')
+lastName  = input('Please enter last name: ')
+
+print('hello ' + firstName + " " + lastName + ' Welcome to the python world')
