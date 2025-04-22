63c9827ea07c696d9e42203191144a63d4bd604c
ssagar87 <103563018+ssagar87@users.noreply.github.com>
2025-04-22 10:12:01 +0530

    Completed second assignment

---
+++ task-3.py
@@ -0,0 +1,7 @@
+
+val = int(input("Enter a number: "))
+
+if val % 2 == 0:
+  print(val,"is an even value")
+else:
+    print(val,"is an odd value")
---
+++ task-4.py
@@ -0,0 +1,6 @@
+totalSum = 0;
+
+for i in range(1, 51):
+    totalSum += i   
+    
+print("Total sum of numbers from 1 to 50 is: ",totalSum)
